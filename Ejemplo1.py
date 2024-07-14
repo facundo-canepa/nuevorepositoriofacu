e77fcf04c267524909a5e2d17c2d0e2c801edcbe
Facundo Cánepa <facundo.villalba.canepa@gmail.com>
2024-07-14 18:59:47 -0300

    agrego dos funciones más

--- Ejemplo1.py
+++ Ejemplo1.py
@@ -1,2 +1,5 @@
 def mensaje1():
     print("mi primera version after")
+
+def mensaje3():
+    print("mi mensaje 3")
--- Ejemplo2.py
+++ Ejemplo2.py
@@ -1,2 +1,5 @@
 def mensaje2():
     print("estoy trabajando con git")
+
+def mensaje4():
+    print("mi mensaje a 4")
